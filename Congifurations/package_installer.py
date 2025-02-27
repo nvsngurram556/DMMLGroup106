deef412e3010d49829ed490e6e642f5f6a888094
Gurram Naga Venkata Satya Narayana <nvsngurram@gmail.com>
2025-02-28 02:27:38 +0530

    code updated with logging details

--- Congifurations/package_installer.py
+++ Congifurations/package_installer.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.check_call(["pip", "install", "pandas", "numpy", "requests", "schedule" ,"psycopg2"])
+subprocess.check_call(["pip", "install", "pandas", "numpy", "requests", "schedule" ,"psycopg2", "logging", "os", "sys", "datetime"])
