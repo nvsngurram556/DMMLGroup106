e5755d54dfff5fcc1185431e0092d79bde65f577
Gurram Naga Venkata Satya Narayana <nvsngurram@gmail.com>
2025-03-01 03:07:04 +0530

    DataIngestion task sync

--- Congifurations/package_installer.py
+++ Congifurations/package_installer.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.check_call(["pip", "install", "pandas", "numpy", "requests", "schedule" ,"psycopg2", "logging", "os", "sys", "datetime"])
+subprocess.check_call(["pip", "install", "pandas", "numpy", "requests", "schedule", "psycopg2", "kaggle","kagglehub[pandas-datasets]"])
