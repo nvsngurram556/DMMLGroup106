0fa53feb000ff6a1a34bfa071dfb0372bc59cb6f
Gurram Naga Venkata Satya Narayana <nvsngurram@gmail.com>
2025-03-11 00:38:16 +0530

    feature store update

--- FeatureStore/Feature_Store.py
+++ FeatureStore/Feature_Store.py
@@ -1,16 +1,22 @@
 import subprocess
 import pandas as pd
 import os
+import logging
+import sys
 from feast import FeatureStore
 from feast.infra.offline_stores.file_source import SavedDatasetFileStorage
 
+# Logging configuration
 date_time = pd.Timestamp.now().strftime("%Y%m%d%H%M%S")
+log_file = f'logs/feature_Store_{date_time}.log'
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
 def initiate_feature_store():
     # Ensure Feast is installed and initialized before running this script
     subprocess.run(["feast", "version"])
-     
     # Initialize Feast repository
-    subprocess.run(["feast", "init", "-m", "feature_repo"], cwd=".")     
+    subprocess.run(["feast", "init", "-m", "feature_repo"], cwd=".")   
+    logging.info("Feature Store initiated")  
 
 def getTransformedData():
     # Load the transformed data
@@ -32,16 +38,14 @@ def getTransformedData():
     target_df.to_parquet(path='FeatureStore/feature_repo/feature_repo/data/target_df.parquet')
     
     #print(predictors_df.describe())
+    logging.info("Transformed data loaded")  
  
  
 def historicalFeaturesFromFeatureStore():
-    
     # Initialize FeatureStore
     store = FeatureStore(repo_path='FeatureStore/feature_repo/feature_repo')
-     
     # Read entity DataFrame from a Parquet file
     entity_df = pd.read_parquet(path='FeatureStore/feature_repo/feature_repo/data/target_df.parquet')
-     
     # Get historical features from the FeatureStore
     training_df = store.get_historical_features(
         entity_df=entity_df,
@@ -52,16 +56,19 @@ def historicalFeaturesFromFeatureStore():
             "customer_df_feature_view:monthlycharge",
             "customer_df_feature_view:avgmonthlygbdownload",
             "customer_df_feature_view:churnlabel",
-            
+            "customer_df_feature_view:avgmonthlylongdistancecharges",
+            "customer_df_feature_view:customerid",
+            "customer_df_feature_view:customer_tenure_months",
+            "customer_df_feature_view:customers_all_type_services",
+            "customer_df_feature_view:total_spent_bycustomer_yearly",
         ]
     ).to_df()
-    
     #dataset = store.create_saved_dataset(from_=training_data, name = "customer_churn_dataset", storage=SavedDataFileStorage('data/customer_churn_dataset.parquet'))
     #training_data
-     
     training_df.to_csv(f'Outputfiles/customer_churn_dataset_{date_time}.csv')
     print("Dataset created")
-    
+    logging.info("Historical features created")  
+
 if __name__=='__main__':
     #initiate_feature_store()
     if os.makedirs('FeatureStore/feature_repo', exist_ok=True):
@@ -74,6 +81,6 @@ if __name__=='__main__':
     
     # Apply Feast configuration
     subprocess.run(["feast", "apply"], cwd="FeatureStore/feature_repo/feature_repo")
-    
+    logging.info("Applied Feast configuration")
     historicalFeaturesFromFeatureStore()
 
